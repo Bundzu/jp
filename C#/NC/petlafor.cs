72315218e6820d5d786e2096f380ac7b749908a0
Bundzu <bundz91@gmail.com>
2013-02-03 18:40:59 +0100

    pętla for

---
+++ C#/NC/petlafor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+		for (int i=0;i <= 1000 ; i=i+10)
+            {
+                Console.WriteLine(i); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/petlafor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+
+            //Typowe zastosowanie pętli for: wypisyjemy co dziesiątą liczbę od 0 to 1000 
+            for (int i=0;i <= 1000 ; i=i+10)
+            // Pierwsze wyrażenie to wartość zmiennej "i" która wynosi 0 ;
+       		// drugie wyrażenie to warunek wyjścia po którym dziłanie pętli się kończy (liczba mniejsza lub równa 1000) ;
+       		// trzecie wyrażenie to to co robi się przy każdym przejściu pętli (w tym wypadku wartośc zmiennej "i" rośnie co 10) ; 
+            {
+                Console.WriteLine(i); // tutaj wypisane są wszystkie liczby z pętli
+            }
+            Console.ReadLine(); 	  // naciskamy enter aby zamknąć program
+        }
+    }
+}
